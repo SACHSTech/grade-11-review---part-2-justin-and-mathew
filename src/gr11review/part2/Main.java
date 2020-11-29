838357e71054c1fabd145ce5a4dc09d5d5959947
Justin Chan <justin.chan21@ycdsbk12.ca>
2020-11-29 21:49:23 +0000

    completed Methods 1

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,16 @@
 package gr11review.part2;
 
+import gr11review.part2.Utility.*;
+import java.io.*;
+
+public class Main{
+  public static void main (String[] args) throws IOException{
+
+    String strMiddleRemover;
+
+    strMiddleRemover = Utility.zipZap("ziiiipzap");
+
+    System.out.println(strMiddleRemover);
+
+  }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,5 +1,34 @@
 package gr11review.part2;
 
 public class Utility {
-    
+  public static String zipZap(String strWord){
+
+    // Create a method that for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+
+    // zipZap("zipXzap") → "zpXzp"
+    // zipZap("zopzop") → "zpzp"
+    // zipZap("zzzopzop") → "zzzpzp"
+    // zipZap("zzzuzoz") → "zzzuzoz"
+
+    int intCount;
+    String strNewWord;
+
+    strNewWord = "";
+
+    if(strWord.length() <= 2){
+      return strWord;
+    }
+
+    for(intCount = 0; intCount < strWord.length(); intCount++){
+      if(strWord.substring(intCount, intCount + 1).equalsIgnoreCase("z") && strWord.substring(intCount + 2, intCount + 3).equalsIgnoreCase("p")){
+        strNewWord = strNewWord + strWord.substring(intCount, intCount + 1) + strWord.substring(intCount + 2, intCount + 3);
+        intCount = intCount + 2;
+      }else{
+        strNewWord = strNewWord + strWord.substring(intCount, intCount + 1);
+      }
+    }
+
+    return strNewWord;
+
+  }
 }
